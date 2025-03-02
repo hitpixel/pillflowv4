5938e7e5b825c1a8a9dc96856c66661094452ea7
hitpixel <hitpixel.main@gmail.com>
2025-03-02 13:35:35 +0000

    v1.3

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://jcnbjdptespaklszrafq.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjbmJqZHB0ZXNwYWtsc3pyYWZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4ODY5OTksImV4cCI6MjA1NjQ2Mjk5OX0.5K8HYuKpYbxXlVQSqTUGZ5DBKfC7M5k1sXJT2da6aKo";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
